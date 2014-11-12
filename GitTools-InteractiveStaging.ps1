36c9686b0ac6522a687f0825500df4c6595e3028
Shaun Luttin <shaunluttin@gmail.com>
2014-11-12 08:38:52 -0800

    Start on interactive staging.

---
+++ GitTools-InteractiveStaging.ps1
@@ -0,0 +1,26 @@
+﻿
+ni inter.txt -t f
+ac inter.txt "One"
+
+<# 
+
+run interactive shell outside of the ISE!
+out of the box,
+powershell ISE doesn't work in interactive mode :-(
+
+#>
+
+git add -i
+
+<#
+
+status
+update, choose files to stage
+revert, choose files to unstage
+add untracked
+patch
+diff, choose files to view staged diff
+quit
+help 
+
+#>
