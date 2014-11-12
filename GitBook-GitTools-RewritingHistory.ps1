4d5520304ae39db400ca1edbf6e163a83846f47f
Shaun Luttin <shaunluttin@gmail.com>
2014-11-12 09:22:42 -0800

    Continue with rewriting history.

---
+++ GitBook-GitTools-RewritingHistory.ps1
@@ -0,0 +1,10 @@
+git commit --amend
+-avoid if you've already pushed
+-change the commit message, or
+-change the snapshot with dd or m"
+
+-change the snapshot with `add` or `rm`
+-warning: is a mini-rebase, changes the SHA
+
+git rebase -i
+-supply the parent of the last commit you want to edit
