cf4f4a0fdc2f998be495d99bcefa53fc40533a85
Shaun Luttin <shaunluttin@gmail.com>
2014-11-13 10:04:55 -0800

    Add filter-branch documentation

--- GitBook-GitTools-RewritingHistory.ps1
+++ GitBook-GitTools-RewritingHistory.ps1
@@ -6,3 +6,13 @@ git commit --amend
 
 git rebase -i
 -supply the parent of the last commit you want to edit
+
+filter-branch
+- changing email globally
+-remove file from every commit
+-avoid using for pushed files
+-`--tree-filter` checks out each commit, changes it, then recommits the results
+-`--subdirectory-filter` works on directories and implies remap to ancestor
+-`--commit-filter` rewrites commit properties such as GIT_AUTHOR_NAME
+-git filter-branch --tree-filter 'some git command' HEAD
+-use --all to run on all branches
