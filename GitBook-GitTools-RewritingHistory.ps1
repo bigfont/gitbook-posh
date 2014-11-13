de1552ce39197d7f650f537558a03ac6208ee2f1
Shaun Luttin <shaunluttin@gmail.com>
2014-11-13 09:53:15 -0800

    Merge

--- GitBook-GitTools-RewritingHistory.ps1
+++ GitBook-GitTools-RewritingHistory.ps1
@@ -1,16 +1,8 @@
 git commit --amend
 -avoid if you've already pushed
--change the commit message, or
-<<<<<<< HEAD
--change the snapshot with dd or m"
-
--change the snapshot with `add` or `rm`
--warning: is a mini-rebase, changes the SHA
-
-=======
+-change the commit message, or
 -change the snapshot with `add` or `rm`
 -warning: is a mini-rebase, changes the SHA
 
->>>>>>> f297df7b3e31a61c484af4d76d620905fa2f8c54
 git rebase -i
 -supply the parent of the last commit you want to edit
