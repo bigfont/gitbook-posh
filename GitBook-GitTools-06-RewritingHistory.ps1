fa99ef90c1edc576d93420396ef0ac7ab974ffaf
Shaun Luttin <shaunluttin@gmail.com>
2014-11-26 08:24:29 -0800

    Add section numbers

---
+++ GitBook-GitTools-06-RewritingHistory.ps1
@@ -0,0 +1,19 @@
+git commit --amend
+-avoid if you've already pushed
+-change the commit message, or
+-change the snapshot with `add` or `rm`
+-warning: is a mini-rebase, changes the SHA
+
+git rebase -i
+-supply the parent of the last commit you want to edit
+
+filter-branch
+- changing email globally
+-remove file from every commit
+-avoid using for pushed files
+-`--tree-filter` checks out each commit, changes it, then recommits the results
+-`--subdirectory-filter` works on directories and implies remap to ancestor
+-`--commit-filter` rewrites commit properties such as GIT_AUTHOR_NAME
+-git filter-branch --tree-filter 'some git command' HEAD
+-use --all to run on all branches
+
---
+++ GitBook-GitTools-07-ResetDemystified.ps1
@@ -0,0 +1,7 @@
+reset and checkout
+# the three trees
+- tree here is 'collection of files'
+- HEAD, Index, Working Directory
+- HEAD is the snapshot of your last commit
+-- git cat-file -p HEAD
+-- git ls-tree -r HEAD
