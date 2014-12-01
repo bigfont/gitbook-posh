5e9a359704737bd92a3c8a1803519822c3c1c2d1
Shaun Luttin <shaunluttin@gmail.com>
2014-12-01 10:01:16 -0800

    Add outline

--- GitBook-GitTools-08-AdvancedMerging.ps1
+++ GitBook-GitTools-08-AdvancedMerging.ps1
@@ -11,7 +11,7 @@ git config --local core.autocrlf false
 1. create ruby file with initial content
 2. commit in master
 3. switch to whitespace
-4. convert from unix to dos
+4. convert from dos 2 unix (*)
 5. commit
 6. change world to mondo
 7. commit
@@ -19,3 +19,29 @@ git config --local core.autocrlf false
 9. document the ruby
 0. commit on master
 1. try to merge in whitespace to master
+
+## aborting a merge
+
+## ignoring whitespace
+
+## manual file re-merging
+
+## checking out conflicts
+
+## merge log
+
+## combined diff format
+
+# undoing merges
+
+## fix the references
+
+## reverse the commit
+
+# other types of merges
+
+## ours or theirs preference
+
+## subtree merging
+
+## 
