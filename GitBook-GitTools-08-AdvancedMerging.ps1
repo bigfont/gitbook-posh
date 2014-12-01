a8cccb2eef9b5d644600868c5bed13f8a160805d
Shaun Luttin <shaunluttin@gmail.com>
2014-12-01 09:49:49 -0800

    Turn off autocrlf locally

--- GitBook-GitTools-08-AdvancedMerging.ps1
+++ GitBook-GitTools-08-AdvancedMerging.ps1
@@ -4,6 +4,10 @@
 
 # merge conflicts
 
+**To work with the example first turn off autocrlf locally**
+
+git config --local core.autocrlf false
+
 1. create ruby file with initial content
 2. commit in master
 3. switch to whitespace
@@ -11,3 +15,7 @@
 5. commit
 6. change world to mondo
 7. commit
+8. switch to master
+9. document the ruby
+0. commit on master
+1. try to merge in whitespace to master
