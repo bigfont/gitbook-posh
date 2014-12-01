bab5654724c68c6ee9f621dfeb95b1697ed785b9
Shaun Luttin <shaunluttin@gmail.com>
2014-12-01 08:39:40 -0800

    Start on advanced merges

---
+++ GitBook-GitTools-08-AdvancedMerging.ps1
@@ -0,0 +1,9 @@
+
+- in git, it's best to merge often to avoid conflicts
+- git does not try to auto-merge funky conflicts
+
+# merge conflicts
+
+1. commit or stash work in progress
+- i.e. start with a clean working dir
+- this lets us undo *anything* we do
