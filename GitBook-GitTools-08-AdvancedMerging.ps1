7ed86d59351bde12ff6f245bbaaa58d0f8fc1f89
Shaun Luttin <shaunluttin@gmail.com>
2014-12-02 09:33:12 -0800

    Advanced merging notes

--- GitBook-GitTools-08-AdvancedMerging.ps1
+++ GitBook-GitTools-08-AdvancedMerging.ps1
@@ -21,7 +21,6 @@ git config --local core.autocrlf false
 1. try to merge in whitespace to master
 2. PROBLEM :-)
 
-
 ## aborting a merge
 
 - `git status -sb` // short, branch
@@ -39,13 +38,34 @@ git config --local core.autocrlf false
 
 ## manual file re-merging
 
+### conflict stages
+
 - stage1, common ancestor
-- stage2, yours
+- stage2, ours
 - stage3, theirs
+
+### inspect the indexes
+
 - `git show :1:fileName.ext`
 - `git show :2:fileName.ext`
 - `git show :3:fileName.ext`
 
+### get the SHAs
+
+- `git ls-files -u` // -u is unmerged
+
+### file merge
+
+- `git merge-file [current] [base] [other] -p`
+- the `-p` sends the result to stout
+
+### doing a manual merge
+
+- `git show :1:simple.rb > simple.common.rb`
+- `git show :2:simple.rb > simple.ours.rb`
+- `git show :3:simple.rb > siple.theirs.rb`
+- `unix2dos simple.theirs.rb`
+- `git merge-file -p simple.ours.rb simple.common.rb simple.theirs.rb`
 
 ## checking out conflicts
 
